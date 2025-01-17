a70d5816720502820b100413e117473545aba4b0
Jellman86 <179294116+Jellman86@users.noreply.github.com>
2025-01-17 09:34:12 +0000

    cleanup
    code clean up post upload

--- remote-Till-Restarter.ps1
+++ remote-Till-Restarter.ps1
@@ -1,23 +1,19 @@
-# Please specify target machine(s) and if you want to do a phisical reboot.  
-$computersToRestart = @("TILL-REG-02","TILL-SRV-01"); #List of computers to run the script on, can be more than one by adding ,"comp-name","comp-name2" etc.
+param (
+    [string[]]$ComputersToRestart = @("99-reg-02"), #List of computers to run the script on, can be more than one by adding ,"comp-name","comp-name2" etc.
+    [string]$RestartMXatEnd = "yes", # Do you want to reboot the MX(s) listed at the end of the process?
+    [string]$RestartMachines = "yes", # Do you want to reboot the machine?
+    [string]$RestartXstoreIfNoReboot = "No", # Do you want to restart the xstore software if no reboot is requested?
+    [string]$ComputerShutdown = "no" # If your not rebooting the machine do you want to shut it down?
+)
 
-# Options to Choose ------------------------------------------------------------------------------------------
-$restartMXatEnd = "yes"; # Do you want to reboot the MX(s) listed at the end of the process?
-$restartMachines = "yes"; # Do you want to reboot the machine?
-    $restartXstoreIfNoReboot = "No"; # Do you want to restart the xstore software if no reboot is requested?
-    $computerShutdown =  "no"; # If your not rebooting the machine do you want to shut it down?
+# List of Script Variables
+$neededModules = @("PSMeraki"); # Modules needed for the script.
 
-# List of Script Variables -----------------------------------------------------------------------------------
-$neededModules = @("PSMeraki");
-$xstoreAnchors = @('C:\xstore\tmp\xstore.anchor','C:\xstore\tmp\dataserver.anchor','C:\environment\tmp\xenv_eng.anchor','C:\environment\tmp\xenv_ui.anchor','C:\xstore-mobile\tmp\xstore_mobile.anchor','C:\eftlink\tmp\eftlink.anchor');
-
-# Script Proper ----------------------------------------------------------------------------------------------
-
-# Quick way to obvescate the path for github
+# Quick way to obvescate sensitive info for github.
 $environment = (get-content .\rs.env); $env = $null;
 foreach($line in $environment){
     $env += @(
-        [pscustomobject]@{itm= $line.split('=')[0];val=$line.split('=')[1] }
+        [pscustomobject]@{itm=$line.split('=')[0];val=$line.split('=')[1] }
         )
 }
 
@@ -62,14 +58,24 @@ Function Invoke-Restarts {
 
             # Code to be executed on remote machine 
             invoke-command -ComputerName $machine -ScriptBlock {
-                
+
+                #Get all the Anchors
+                write-host "Finding Anchor Files";
+                Try{
+                    $xstoreAnchors = get-childitem -Path @("c:\xstore\tmp","c:\environment\tmp","c:\xstore-mobile\tmp","c:\eftlink\tmp") | Where-Object {$_.Extension -eq ".anchor"} -ea 0;
+                }catch{
+                    Write-Host "Warn: $_ Path not available, if this not a server and the path is mobile, this is fine!" -ForegroundColor White -BackgroundColor Red
+                    return
+                }
+
                 #Delete xStore anchor files
-                ForEach ($anchors in $using:xstoreAnchors){
-                    if (Test-Path $anchors){
-                        Remove-Item $anchors -Force
-                        "$anchors has been deleted on $env:computername."
+                ForEach ($anchor in $xstoreAnchors){
+                    if (Test-Path $anchor.FullName){
+                        $anchorPath = $anchor.FullName
+                        Remove-Item $anchor.FullName -Force
+                        "$anchorPath has been deleted on $env:computername."
                     }else{
-                        "$anchors doesn't exist."
+                        "$anchorPath doesn't exist."
                     }
                 }
                 
@@ -77,7 +83,7 @@ Function Invoke-Restarts {
                 Start-Sleep -Seconds 15
 
                 # get process list to see if xstore is still running. 
-                if((get-process | Where-Object {$_.ProcessName -ilike "xstore*"}).count -lt '1'){
+                if((get-process | Where-Object {$_.ProcessName -ilike "xstore*" -and $_.ProcessName -ilike "xenv*"}).count -lt '1'){
                     "Success Xstore appears to be closed"
                     if($using:restartMachines -ilike "y*"){
                         "Restart Computer has been set to $using:restartMachines, so this machine $env:computername will be restarted."
@@ -103,34 +109,27 @@ Function Invoke-Restarts {
 }
 Function Invoke-MerakiRestarts {
 
-    Remove-Variable storeInfo,merakiApiKeySJP,mxSearchString,mxSerials -EA 0
-
     if($restartMXatEnd -ilike "Y*"){
+
         $storeInfoPath = ($env | Where-Object {$_.itm -ieq "storeInfoPath"}).val;
-        $merakiApiKeySJPPath = ($env | Where-Object {$_.itm -ieq "merakiApiKeySJPPath"}).val;
-            $storeInfo = [xml]$datasource = Get-Content $storeInfoPath;
-            $merakiApiKeySJP = [xml]$datasource = Get-Content $merakiApiKeySJPPath;
+        $merakiApiKeyPath = ($env | Where-Object {$_.itm -ieq "merakiApiKeyPath"}).val;
+        $storeInfo = [xml]$datasource = Get-Content $storeInfoPath;
+        $merakiApiKey = [xml]$datasource = Get-Content $merakiApiKeyPath;
 
-        $mxSearchString = ($computersToRestart[0]).Split('-')[0]
-        $mxSerials = ($storeInfo.stores.store | Where-Object {$_.name -ilike $mxSearchString}).MX.serial
-            if($null -eq $mxSerials -or '' -eq $mxSerials){
-                $mxSerials = ($storeInfo.stores.store | Where-Object {$_.name -ilike "$mxSearchString*"}).MX.serial
-            }
+        $mxSearchString = ($computersToRestart[0]).Split('-')[0];
+        $mxSerials = ($storeInfo.stores.store | Where-Object {$_.name -ilike "*$mxSearchString*"}).MX.serial;
 
-        if(Test-Path $storeInfoPath, $merakiApiKeySJPPath){
-                Set-MrkRestApiKey $merakiApiKeySJP.sitedata.meraki.api.key;
+                Set-MrkRestApiKey $merakiApiKey.sitedata.meraki.api.key;
                 if($null -eq $mxSerials -or '' -eq $mxSerials){
                     $mxSerials = Read-Host -Prompt "You have requested an MX reboot but we cannot match the store name in the datasource, please provide the MX serial in the format (XXX-XXX-XXX)";
                 }
+
                 Foreach($serial in $mxSerials){
                     Write-host "Waiting for 15 Seconds before rebooting the MX, so the restarted machines can get a DHCP lease before the MX is rebooted." -BackgroundColor DarkBlue -ForegroundColor White;
                     #Invoke-Countdown -Seconds 15;
                     $restartResult = Restart-MrkDevice -Serial $serial;
                     "Meraki Reports that success for the MX reboot with the serial ($serial) is: " + $restartResult.success
                 }
-        }else{
-            Write-Host "Cannot find Datasources, you may not have read priviage or not be connected to the corporate network." -BackgroundColor DarkRed -ForegroundColor White;
-        }
     }else{
         Write-Host "No MX reboot requested";
     }
